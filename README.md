a0b240c335f0add98d7437633d00bd30e4246613
Alexandre Souza Pereira <sp.alexandre26@gmail.com>
2025-01-31 15:43:35 -0300

    adiciona script `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone_tabnews
+
 Estudo de como criar um projeto de Zero ao Deploy
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
-export default function Home(){
-    return(<><h1>Ingrid, Bom dia, Juridico do Alexandre aqui. 😎</h1>    <h2>Me da um beijinho? 😘</h2></>)
-
+export default function Home() {
+  return (
+    <>
+      <h1>Ingrid, Bom dia, Juridico do Alexandre aqui. 😎</h1>{" "}
+      <h2>Me da um beijinho? 😘</h2>
+    </>
+  );
 }
