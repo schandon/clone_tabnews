169ec00f137515ace324533ca12cf4a4faea427f
schandon <sp.alexandre26@gmail.com>
2025-02-20 21:19:18 -0300

    Atualização de Status

--- pages/api/status.js
+++ pages/api/status.js
@@ -2,5 +2,4 @@ export default function status(request, response) {
   return response
     .status(200)
     .json({ chave: "Alunos curso.dev são acima da média" });
-  // .send("Alunos curso.dev são acima da média")
 }
