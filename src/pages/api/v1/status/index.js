02a4916a06327ac4267c0f63ed90dfd1feb7418e
schandon <sp.alexandre26@gmail.com>
2025-03-03 06:54:40 -0300

    Finish `api/v1/status` endpoint`

--- src/pages/api/v1/status/index.js
+++ src/pages/api/v1/status/index.js
@@ -9,12 +9,21 @@ export default async function status(request, response) {
   const maxConnetions = await database.query("SHOW max_connections;");
   const maxConnectionsValues = maxConnetions.rows[0].max_connections;
 
+  const databaseName = process.env.POSTGRES_DB;
+  const actiodatabaseOpenedConnectionResult = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const activeDatabaseConnections =
+    actiodatabaseOpenedConnectionResult.rows[0].count;
+
   return response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
       database: {
         version: versionValues,
         max_connetions: parseInt(maxConnectionsValues),
+        opend_connections: activeDatabaseConnections,
       },
     },
   });
