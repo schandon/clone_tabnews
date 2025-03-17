0c9879ffa09906ff0fec12dae9c3313abe9791ba
Alexandre Souza Pereira <sp.alexandre26@gmail.com>
2025-03-17 16:29:41 -0300

    Ajuste Test Get/Post

--- src/tests/integration/api/v1/migrations/get.test.js
+++ src/tests/integration/api/v1/migrations/get.test.js
@@ -13,4 +13,5 @@ test("GET to /api/v1/migrations should return 200", async () => {
   const responseBody = await response.json();
 
   expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
 });
--- src/tests/integration/api/v1/migrations/post.test.js
+++ src/tests/integration/api/v1/migrations/post.test.js
@@ -1,3 +1,10 @@
+import database from "infra/database";
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
 test("POST to  /api/v1/migration should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
@@ -5,6 +12,11 @@ test("POST to  /api/v1/migration should return 200", async () => {
   expect(response.status).toBe(200);
 
   const response_body = await response.json();
+  console.log("response body", response_body);
 
   expect(Array.isArray(response_body)).toBe(true);
+  expect(response_body[0].timestamp).toEqual(1741606460552);
+  expect(response_body[0].path).toContain(
+    "1741606460552_first-migrate-test.js"
+  );
 });
