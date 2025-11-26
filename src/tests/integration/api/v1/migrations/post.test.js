7dfd6a80fe60b558e1af6980746f58f46480dcba
Alexandre Souza Pereira <sp.alexandre26@gmail.com>
2025-11-26 10:42:19 -0300

    fix `prettier`style linting

--- src/tests/integration/api/v1/migrations/post.test.js
+++ src/tests/integration/api/v1/migrations/post.test.js
@@ -2,17 +2,14 @@ import database from "infra/database.js";
 import orcherstrator from "src/tests/orcherstrator.js";
 
 beforeAll(async () => {
-  await orcherstrator.waitForAllServices()
+  await orcherstrator.waitForAllServices();
   await database.query("drop schema public cascade; create schema public;");
 });
 
 test("POST to /api/v1/migrations should return 200", async () => {
-  const response1 = await fetch(
-    "http://localhost:3000/api/v1/migrations",
-    {
-      method: "POST",
-    }
-  );
+  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
   expect(response1.status).toBe(201);
 
   const response1Body = await response1.json();
@@ -20,12 +17,9 @@ test("POST to /api/v1/migrations should return 200", async () => {
   expect(Array.isArray(response1Body)).toBe(true);
   expect(response1Body.length).toBeGreaterThan(0);
 
-  const response2 = await fetch(
-    "http://localhost:3000/api/v1/migrations",
-    {
-      method: "POST",
-    }
-  );
+  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
   expect(response2.status).toBe(200);
 
   const response2Body = await response2.json();
